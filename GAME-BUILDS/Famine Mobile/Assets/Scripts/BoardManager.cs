1793961d3f7f390d3e80a19d1e0a60ae8f5478a5
Sam Giarratana <sam@giarratana.com>
2017-07-26 02:17:19 -0500

    Added Grid
    began boardmanager

---
+++ GAME-BUILDS/Famine Mobile/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    public int halfRows;
+    public int halfColumns;
+    public float verticalDistance;
+    public float horizontalDistance;
+
+    private GameObject[][] Board;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
