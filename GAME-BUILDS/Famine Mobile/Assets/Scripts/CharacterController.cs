9d22560f2019477290357a9c19420f753a3f9535
Sam Giarratana <sam@giarratana.com>
2017-08-01 14:34:11 -0700

    Controller Initialization

---
+++ GAME-BUILDS/Famine Mobile/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public string Name;
+    public string Family;
+    public int Level;
+    public string Race;
+    public bool Sex;
+
+    public int xPos;
+    public int yPos;
+
+    public int Health;
+    public int Resource;
+    public string ResourceName;
+
+    public Action[] Actions;
+
+    public int Vigor;
+    public int Faith;
+    public int Intellect;
+    public int Dexterity;
+    public int Strength;
+    public int Wisdom;
+
+    public float Fame;
+    public float Divinity;
+    public float Renown;
+
+    public int GoldPieces;
+    public int SilverPieces;
+    public int CopperPieces;
+
+    public GameObject[] Effects;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void UseAbility(int num)
+    {
+        Actions[num].Execute();
+    }
+}
--- GAME-BUILDS/Famine Mobile/Assets/Scripts/PlayerController.cs
+++ GAME-BUILDS/Famine Mobile/Assets/Scripts/PlayerController.cs
@@ -1,56 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-//using System.Threading.Tasks;
-using System.IO;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public string filePath;
 
-    string GenInfo;
-    string StatInfo;
-    string CurrencyInfo;
-
-    string ActionsInfo;
-    string AbilityInfo;
-
-    string EqpInfo;
-    string InvInfo;
-
-    string WorldInfo;
-    string LineageInfo;
-
+    public CharacterController character;
 	// Use this for initialization
-	void Start ()
-    {
-        List<string> lines = File.ReadAllLines(filePath).ToList();
-
-        GenInfo = lines[0];
-        StatInfo = lines[1];
-        CurrencyInfo = lines[2];
-
-        ActionsInfo = lines[3];
-        AbilityInfo = lines[4];
-
-        EqpInfo = lines[5];
-        InvInfo = lines[6];
-
-        WorldInfo = lines[7];
-        LineageInfo = lines[8];
+	void Start () {
+		
 	}
 	
 	// Update is called once per frame
-	void Update ()
-    {
+	void Update () {
 		
 	}
 
-    private void takeTurn()
+    public void MoveTwelve()
     {
-       // GameObject slots = GameObject.Find <'ActionsUI'> ();
-       // slots.GetComponent < "ActionsController" > ().newTurn();
+        character.xPos = character.xPos + 1;
+        character.yPos = character.yPos + 2;
+    } 
+
+    public void MoveOneThirty()
+    {
+        character.xPos = character.xPos + 1;
+        character.yPos = character.yPos + 1;
+    }
+
+    public void MoveFourThirty()
+    {
+        character.xPos = character.xPos + 1;
+        character.yPos = character.yPos - 1;
+    }
+
+    public void MoveSix()
+    {
+        character.xPos = character.xPos - 1;
+        character.yPos = character.yPos -2 ;
+    } 
+
+    public void MoveSevenThirty()
+    {
+        character.xPos = character.xPos - 1;
+        character.yPos = character.yPos - 1;
+    }
+
+    public void MoveTenThirty()
+    {
+        character.xPos = character.xPos - 1;
+        character.yPos = character.yPos + 1;
+    }
+
+    public void AbilityOne(int num)
+    {
+        character.UseAbility(num);
     }
 }
