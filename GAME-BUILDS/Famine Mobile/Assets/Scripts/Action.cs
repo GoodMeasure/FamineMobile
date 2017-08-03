0f10129c48dd64e48a046dec6f7d5add505e8e94
Sam Giarratana <sam@giarratana.com>
2017-08-03 00:27:34 -0700

    Abstract Experimentation

--- GAME-BUILDS/Famine Mobile/Assets/Scripts/Action.cs
+++ GAME-BUILDS/Famine Mobile/Assets/Scripts/Action.cs
@@ -5,4 +5,14 @@ using UnityEngine;
 public abstract class Action : MonoBehaviour
 {
     public abstract void Execute();
+    public bool Targeted;
+    public bool Numeric;
+    public bool isString;
+    public GameObject pc;
+    public void Start()
+    {
+        pc = GameObject.Find("PlayerCharacter");
+    }
+
+    public RangeCheck()
 }
